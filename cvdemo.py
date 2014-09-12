110ad2644e517b90cb4e65d75850ac3761b4e35d
haxwithaxe <me@haxwithaxe.net>
2014-09-12 01:49:41 -0400

    maybe satisfactory version of human finder

--- cvdemo.py
+++ cvdemo.py
@@ -19,14 +19,14 @@ class HumanFinder:
         self.img = None
         self.background = background
         self.color_map_start = color_map_start
-        self.cm_start_color = Color.RED
+        self.cm_colors = (Color.MAYBE_FOREGROUND) #(Color.RED,(128,0,0),(128,128,0),(0,255,0),(0,128,128),(0,0,128),Color.BLUE)
         self.color_map_end = color_map_end
-        self.cm_end_color = Color.BLUE
         self.show = show
-        #self.mask = self.cam.getImage().grayscale()
+        self.mask = None
 
     def _pre_process_img(self, img):
-        return (img.binarize().invert() - img.skeletonize())
+        ''' do things to the image before sending it through the blob finders '''
+        return img.grayscale()
 
     def getImage(self):
         ''' Get the image from a camera '''
@@ -37,6 +37,8 @@ class HumanFinder:
         else:
             # otherwise just grab an image from the camera
             self.img = self._pre_process_img(self.cam.getImage())
+        #self.img.show()
+        #raw_input()
 
     def getMotionImage(self):
         ''' Get a time separated composite image that shows only things that are different between the two. '''
@@ -51,6 +53,16 @@ class HumanFinder:
         '''
         return self.img.findBlobs()
 
+    def smartGetBlobs(self, dumb_blobs):
+        ''' Get blobs from self.img 
+        @returns        List of blobs or None if no blobs.
+        '''
+        self.getMaskFromBlobs(dumb_blobs)
+        #self.img.show()
+        #raw_input()
+        #print(self.img.height, self.img.width)
+        return self.mask.findBlobs()
+
     def drawBlobs(self, blobs, color_map):
         ''' Draw blobs over self.img for display purposes.
         @param  blobs       List of blobs
@@ -62,12 +74,24 @@ class HumanFinder:
                # This eventually throws a division by zero error in Color.py while calculating colordistance
                 ba = blob.area()
                 if ba > color_map.startmap:
-                    cm = color_map[ba]
-                    blob.draw(cm)
+                    try:
+                        cm = color_map[ba]
+                        blob.draw(cm)
+                    except: #DivideByZeroError as e:
+                        pass
 
-    def find_humans(self):
-        pass
-
+    def getMaskFromBlobs(self, dumb_blobs):
+        mask = Image((self.img.width, self.img.height))
+        mask.drawRectangle(x=0, y=0, h=mask.height, w=mask.width, color=Color.BACKGROUND, alpha=0)
+        #dumb_blobs.reassignImage(mask)
+        for x in dumb_blobs:
+            mask = mask.logicalOR(x.getFullHullMaskedImage(), grayscale=False)
+        mask.applyLayers()
+        mask.mergedLayers()
+        #mask.show()
+        #raw_input()
+        self.mask = mask
+ 
     def getColorMap(self, blobs):
         ''' Generate a ColorMap based on the blobs found
         @param  blobs       List of blobs
@@ -82,13 +106,12 @@ class HumanFinder:
                 color_map_start = min(blobs.area())
             else:
                 color_map_start = self.color_map_start
-            print(color_map_start)
-            return ColorMap(color=self.cm_start_color, startmap=color_map_start, endmap=color_map_end)
+            return ColorMap(color=self.cm_colors, startmap=color_map_start, endmap=color_map_end)
 
     def drawImage(self):
         ''' Draw self.img on the screen. Disabled if show=False. '''
         if self.show:
-            self.img.show()
+            self.mask.show()
 
     def on_start(self):
         ''' Main loop '''
@@ -96,14 +119,21 @@ class HumanFinder:
             # get image
             self.getImage()
             # find blobs
-            blobs = self.getBlobs()
+            dumb_blobs = self.getBlobs()
+            dumb_blobs.draw()
+            #self.img.show()
+            #raw_input()
+            blobs = self.smartGetBlobs(dumb_blobs)
             if blobs:
+                print(len(blobs)) #,[int(x.radius()) for x in blobs])
                 # make color map
-                color_map = self.getColorMap(blobs)
-                # draw colorized blobs
-                self.drawBlobs(blobs, color_map)
-            # make teh pretty pictures
-            self.drawImage()
+                if self.show:
+                    color_map = self.getColorMap(blobs)
+                    # draw colorized blobs
+                    self.drawBlobs(blobs, color_map)
+            if self.show:
+                # make teh pretty pictures
+                self.drawImage()
 
 if __name__ == '__main__':
     hf = HumanFinder(motion=False, show=True)
