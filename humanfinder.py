8ef2177c3d450970d79152485c4abc514f0c7ea8
haxwithaxe <me@haxwithaxe.net>
2014-09-30 03:12:41 -0400

    added actor model threading and http interface (ra.py). recalibration functionality is untested.

---
+++ humanfinder.py
@@ -0,0 +1,219 @@
+from SimpleCV import *
+import pykka
+import time
+import collections
+import logging
+
+logger = logging
+logger.basicConfig(level=logging.INFO)
+
+class HumanFinder(pykka.ThreadingActor):
+    def __init__(self, parent=None, motion=False, clean_plate=None, show=False, blob_min_radius=30, motion_min_radius=30, min_motion_buffer_len=10, max_1_meatbag_area=11):
+        '''
+        @param  motion                  Boolean, recalibrate based on motion.
+        @param  clean_plate             background image to remove the scene background from captured images Default:None
+        @param  show                    Boolean, draw images on screen if True otherwise don't draw images. Default:False
+        @param  blob_min_radius         Integer, minimum radius of a blob to qualify as big. Default: 30
+        @param  motion_max_radius       Integer, minimum radius of a blob to qualify as not noise in motion detection. Default: 30
+        @param  max_1_meatbag_area      Integer, maximum percentage of total image area expected per human.
+        '''
+        super(HumanFinder, self).__init__()
+        self.parent = parent
+        self.motion_update = motion
+        self.show = show
+        self.blob_min_radius = blob_min_radius
+        self.motion_min_radius = motion_min_radius
+        self.max_1_meatbag_area = max_1_meatbag_area/100.0
+        self.clean_plate = clean_plate
+        self.min_motion_buffer_len = min_motion_buffer_len
+        self.cam = FakeCamera()
+        self.img = None
+        self.last_img = None
+        self.cm_colors = (Color.RED,(128,0,0),(128,128,0),(0,255,0),(0,128,128),(0,0,128),Color.BLUE)
+        self.recalibrate_interval = 60 # minutes
+        self.recalibrated_last = time.time()
+        self.motion_buffer = 0
+        self._count_buffer = []
+        self.thread_delay = 1
+        logger.debug('initialized')
+
+    def _pre_process_img(self, img):
+        ''' do things to the image before sending it through the blob finders '''
+        logger.debug('pre-processing image: %s, %s' % (img, self.clean_plate))
+        return (self.clean_plate.grayscale() - img.grayscale())
+
+    def getImage(self):
+        ''' Get the image from a camera '''
+        logger.debug('getImage')
+        # if self.motion_update is True, attempt to get a fresh clean_plate based on conditions
+        if self.motion_update:
+            # if self.motion_update
+            self.last_img = self.img
+            self.recalibrate()
+        # otherwise just grab an image from the camera
+        self.img = self._pre_process_img(self.cam.getImage())
+        if self.show:
+            self.img.show()
+            raw_input()
+        logger.debug('getImage')
+
+    def seesMotion(self):
+        ''' Determine if any movement is visible between the last and current image and return True if there is.
+        @returns        True if motion is detected, False if not.
+        '''
+        logger.debug('seesMotion')
+        diff_blobs = (self.img-self.last_img).findBlobsFromWatershed()
+        return len([x for x in diff_blobs if x.radius() > self.motion_min_radius]) == 0
+
+    def getBlobs(self):
+        ''' Get blobs from self.img
+        @returns        List of blobs or None if no blobs.
+        '''
+        magic_threshold = 70
+        magic_dialate = 3
+        logger.debug('getBlobs')
+        mask = self.img.threshold(magic_threshold).dilate(magic_dialate)
+        blobs = self.img.findBlobsFromWatershed(mask)
+        if self.show:
+            blobs.show()
+            raw_input()
+        return blobs
+
+    def drawBlobs(self, blobs, color_map):
+        ''' Draw blobs over self.img for display purposes.
+        @param  blobs       List of blobs
+        @param  color_map   ColorMap object
+        '''
+        logger.debug('drawBlobs')
+        # draw colorized blobs
+        if self.show:
+            logger.debug('show == True, drawing blobs, count %d' % len(blobs))
+            for blob in blobs:
+               # This eventually throws a division by zero error in Color.py while calculating colordistance
+                ba = blob.area()
+                if ba > color_map.startmap:
+                    try:
+                        cm = color_map[ba]
+                        blob.draw(cm)
+                    except DivideByZeroError as e:
+                        pass
+
+    def getColorMap(self, blobs):
+        ''' Generate a ColorMap based on the blobs found
+        @param  blobs       List of blobs
+        @returns            ColorMap object or None if disabled
+        '''
+        logger.debug('getColorMap')
+        if self.show:
+            logger.debug('show == True, getting color map')
+            color_map_end = max(blobs.area())
+            color_map_start = min(blobs.area())
+            return ColorMap(color=self.cm_colors, startmap=color_map_start, endmap=color_map_end)
+
+    def drawImage(self):
+        ''' Draw self.img on the screen. Disabled if show=False. '''
+        logger.debug('drawImage')
+        if self.show:
+            logger.debug('show == True, drawing image')
+            self.img.show()
+            raw_input()
+
+    def on_start(self):
+        ''' Main loop 
+            named with the intent of making this module a pykka.Actor
+        '''
+        logger.debug('main loop')
+        while True:
+            logger.debug('---------------------------------loop start')
+            # throttle loop
+            time.sleep(self.thread_delay)
+            # get image
+            self.getImage()
+            # find blobs
+            blobs = self.getBlobs()
+            blobs_big = [x for x in blobs if x.radius() > self.blob_min_radius]
+            if blobs_big:
+                # make color map
+                if self.show:
+                    color_map = self.getColorMap(blobs)
+                    # draw colorized blobs
+                    self.drawBlobs(blobs, color_map)
+            if self.show:
+                # make teh pretty pictures
+                self.drawImage()
+            logger.info('big blob count: %d of %d' % (len(blobs_big), len(blobs)))
+            logger.debug('big blobs area %%: %s' % str([x.area()/self.img.area() for x in blobs_big]))
+            count=0
+            for blob in blobs_big:
+                if blob.area() > self.max_1_meatbag_area:
+                    count+=int((blob.area()/self.img.area())/self.max_1_meatbag_area)
+                else:
+                    count+=1
+            logger.info('said "%d" to parent' % count)
+            self.parent.tell({'update':count})
+            logger.debug('meatbags: %d' % count)
+
+    def recalibrate(self):
+        if time.time() - self.recalibrated_last > 60*60:
+            if self.seesMotion():
+                # we see movement so we'll try again later
+                self.motion_buffer = 0
+                return None
+            elif self.motion_buffer >= self.buffer_len:
+                self.motion_buffer=0
+                self.recalibrated_last = time.time()
+                self.clean_plate = self.cam.getImage()
+            else:
+                self.motion_buffer+=1
+
+class FakeCamera(object):
+
+    _clean_plate = 'clean_plate.jpg'
+    img_path = 'sample-2.jpg'
+    scale_y = 480
+    scale_x = 600
+
+    def clean_plate(self):
+        logger.debug('clean_plate')
+        logger.debug('self._clean_plate: %s' % self._clean_plate)
+        return Image(self._clean_plate).scale(self.scale_x, self.scale_y)
+
+    def getImage(self):
+        logger.debug('getImage')
+        return Image(self.img_path).scale(self.scale_x, self.scale_y)
+
+class HFHandler(pykka.ThreadingActor):
+
+    def __init__(self, **kwargs):
+        super(HFHandler, self).__init__()
+        self.kwargs = kwargs
+        self.kwargs.update({'parent':self.actor_ref})
+        self.buff_len = 10
+        self.buff = collections.deque(maxlen=self.buff_len)
+
+    def on_start(self):
+        self.hf = HumanFinder.start(**self.kwargs)
+
+    def on_receive(self, message):
+        if 'update' in message:
+            logger.info('heard "%d" from child' % message.get('update'))
+            self.append(message.get('update'))
+        else:
+            return self.sample()
+
+    def sample(self):
+        if self.buff:
+            avg = sum(self.buff)/len(self.buff)
+            self.buff.clear()
+            return avg
+        else:
+            return -1
+
+    def append(self, value):
+        self.buff.append(value)
+
+
+if __name__ == '__main__':
+    hf = HFHandler.start(show=True, clean_plate=FakeCamera().clean_plate())
+    time.sleep(30)
+    logger.info('sample: %s' % hf.ask({'a':'A'}))
---
+++ ra.py
@@ -0,0 +1,43 @@
+import SimpleHTTPServer
+import SocketServer
+import sys
+import logging
+import humanfinder
+from humanfinder import FakeCamera
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+PORT = int(sys.argv[1], 10)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('http')
+hf_args = {'show':False, 'clean_plate':FakeCamera().clean_plate()}
+hf = humanfinder.HFHandler.start(**hf_args)
+
+class HumansFound(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logger.debug('received GET request')
+        reply = hf.ask({'sample':0})
+        logger.debug('replying: %s' % reply)
+        f = StringIO()
+        f.write(str(reply))
+        length = f.tell()
+        f.seek(0)
+        self.send_response(200)
+        encoding = 'utf-8'
+        self.send_header("Content-type", "text/html; charset=%s" % encoding)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()
+
+handler = HumansFound
+httpd = SocketServer.TCPServer(('', PORT), handler)
+logger.debug('serving at port: %d' % PORT)
+httpd.serve_forever()
